8619c786d1bae30f586a7899b9233c1c7b4bbc96
Jake VanderVaate <jake.vandervaate@protonmail.com>
2021-02-21 13:00:53 -0600

    fixed website variable

--- sitesync.sh
+++ sitesync.sh
@@ -24,9 +24,9 @@ done
 
 rsync -a $assets/* $dir/ --exclude *.html
 
-sed -i "s|PATHTOSOURCE|$wsd/$dir|g" $wsd/$dir/*.html && sed -i -e 's|PATHTOSOURCE|..|g' -e "s|f='sty|f='../sty|g" $wsd/$dir/*/*.html 2>/dev/null || :
+sed -i "s|PATHTOSOURCE/||g" $wsd/$dir/*.html && sed -i -e 's|PATHTOSOURCE|..|g' -e "s|f='sty|f='../sty|g" $wsd/$dir/*/*.html 2>/dev/null || :
 
 case $1 in
 	demo) : ;;
-	deploy) rsync -avz --delete $public/ $user@website:$vpsdir/ ;;
+	deploy) rsync -avz --delete $public/ $user@$website:$vpsdir/ ;;
 esac
