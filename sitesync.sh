c7edc7957136aff6cb04737e7b78e48f59858ed5
Jake VanderVaate <jake.vandervaate@protonmail.com>
2021-02-20 18:15:20 -0600

    Added sitesync.sh and site directories

---
+++ sitesync.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+cd $2 && wsd="$(pwd)" 
+demo="$wsd/demo"
+content="$wsd/content"
+public="$wsd/public"
+assets="$wsd/assets"
+
+. $wsd/config
+
+case $1 in
+	"demo") dir="local" ;;
+	"deploy") dir="public" ;;
+esac
+
+rsync -a --delete $content/ $dir/
+
+for file in $(find $dir -iname '*.html'); do 
+	pagecontent=$(cat $file)
+	name=$(echo $file | sed "s|$content/||g")
+	cat $assets/header.html > $name && echo $pagecontent >> $name && cat $assets/footer.html >> $name
+
+done
+
+rsync -a $assets/* $dir/ --exclude *.html
+
+sed -i "s|PATHTOSOURCE|$wsd/$dir|g" $wsd/$dir/*.html && sed -i -e 's|PATHTOSOURCE|..|g' -e "s|f='sty|f='../sty|g" $wsd/$dir/*/*.html 2>/dev/null || :
+
+case $1 in
+	demo) : ;;
+	deploy) rsync -avz --delete $public/ $user@website:$vpsdir/ ;;
+esac
