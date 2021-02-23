0b266d1ecad82c9aeffff6d0c742b0b68e3932c2
Jake VanderVaate <jake.vandervaate@protonmail.com>
2021-02-22 20:44:10 -0600

    minor optimizations to prevent breakage; removed -z flag from deploy's rsync

--- sitesync.sh
+++ sitesync.sh
@@ -1,32 +1,29 @@
 #!/bin/sh
 
-cd $2 && wsd="$(pwd)" 
-demo="$wsd/demo"
+cd "$2" && wsd="$(pwd)" 
 content="$wsd/content"
-public="$wsd/public"
 assets="$wsd/assets"
 
-. $wsd/config
+. "$wsd"/config
 
-case $1 in
+case "$1" in
 	"demo") dir="local" ;;
 	"deploy") dir="public" ;;
 esac
 
-rsync -a --delete $content/ $dir/
+rsync -a --delete "$content"/ "$dir"/
 
-for file in $(find $dir -iname '*.html'); do 
-	pagecontent=$(cat $file)
-	name=$(echo $file | sed "s|$content/||g")
-	cat $assets/header.html > $name && echo $pagecontent >> $name && cat $assets/footer.html >> $name
-
+for file in $(find "$dir" -iname '*.html'); do 
+	pagecontent=$(cat "$file")
+	name=$(echo "$file" | sed "s|""$content""/||g")
+	cat "$assets"/header.html > "$name" && echo "$pagecontent" >> "$name" && cat "$assets"/footer.html >> "$name"
 done
 
-rsync -a $assets/* $dir/ --exclude *.html
+rsync -a "$assets"/* "$dir"/ --exclude ./*.html
 
-sed -i "s|PATHTOSOURCE/||g" $wsd/$dir/*.html && sed -i -e 's|PATHTOSOURCE|..|g' -e "s|f='sty|f='../sty|g" $wsd/$dir/*/*.html 2>/dev/null || :
+sed -i "s|PATHTOSOURCE/||g" "$wsd"/"$dir"/*.html && sed -i -e 's|PATHTOSOURCE|..|g' -e "s|f='sty|f='../sty|g" "$wsd"/"$dir"/*/*.html 2>/dev/null || :
 
-case $1 in
+case "$1" in
 	demo) : ;;
-	deploy) rsync -avz --delete $public/ $user@$website:$vpsdir/ ;;
+	deploy) rsync -az --delete "$wsd"/public/ "$user"@"$website":"$vpsdir"/ ;;
 esac
